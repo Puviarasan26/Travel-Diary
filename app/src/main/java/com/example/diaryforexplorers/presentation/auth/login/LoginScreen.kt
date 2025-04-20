35bf3cad0c81b0562d80e3783490c57a1198f113
sppuviarasan <puviek7@gmail.com>
2025-04-20 22:48:28 +0530

    Add navigation setup

--- app/src/main/java/com/example/diaryforexplorers/presentation/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/diaryforexplorers/presentation/auth/login/LoginScreen.kt
@@ -1,15 +1,32 @@
 package com.example.diaryforexplorers.presentation.auth.login
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.example.diaryforexplorers.presentation.navigation.Screen
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LoginScreen(navHostController: NavHostController) {
-    Text(text = "Login Screen", modifier = Modifier.clickable {
-        navHostController.navigate(Screen.Register.route)
-    })
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Login in to your personal diary",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(8.dp),
+            fontFamily = FontFamily.Serif
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(rememberNavController())
 }
